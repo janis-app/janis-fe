f2702d5def269afb711e0e1a55188bc5979a32c1
Murtaxa-Baig <darvaish49@gmail.com>
2023-11-15 21:59:58 +0500

    Dashborad screen home screen and setup-profile screen

--- components/InformationGathering/Header.jsx
+++ components/InformationGathering/Header.jsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { PiCaretLeftBold } from "react-icons/pi";
 
-function Header({ progess, link, show }) {
+function Header({ progess, link, show, title , profile }) {
   const router = useRouter();
   return (
     <div className="pt-[24px] flex justify-between items-center  mb-[32px]">
@@ -12,15 +13,33 @@ function Header({ progess, link, show }) {
       >
         <PiCaretLeftBold size={20} />
       </div>
-      {show && (
-        <div className="bg-[#F7F7F7] h-[8px] w-[128px] relative rounded-2xl">
-          <div
-            className={`absolute inset-0 bg-[#B9E6F5] rounded-2xl`}
-            style={{ width: `${progess}%` }}
-          />
-        </div>
-      )}
-      <div></div>
+      <div>
+        {show && (
+          <div className="bg-[#F7F7F7] h-[8px] w-[128px] relative rounded-2xl">
+            <div
+              className={`absolute inset-0 bg-[#B9E6F5] rounded-2xl`}
+              style={{ width: `${progess}%` }}
+            />
+          </div>
+        )}
+
+        {
+          title && <h2 style={{ color:"#fff"}}>{title}</h2>
+        }
+
+      </div>
+      <div>
+        {
+          profile && 
+          <Image
+          src={profile}
+          width={32}
+          height={32}
+          alt="Profile image"
+          className="rounded-2xl"
+        />
+        }
+      </div>
     </div>
   );
 }
---
+++ pages/home.js
@@ -0,0 +1,71 @@
+import PageHeader from "@/components/Common/PageHeader";
+import Image from "next/image";
+import logo from "../public/assets/logo.png";
+import { LiaEditSolid } from "react-icons/lia";
+import { FaArrowRightLong } from "react-icons/fa6";
+import styles from '@/styles/setup-profile/setup-profile.module.css'
+import profile from '@/public/assets/profileImg.svg'
+import mark from "@/public/assets/mark.svg"
+
+export default function Home() {
+    return (
+        <div className={styles.main_container}>
+
+            <div className={styles.conatiner}>
+                {/* <main className="overflow-x-hidden pt-[30px]"> */}
+                <div className={`h-3/4  pb-[62px] ${styles.logo_container}`}>
+                    <div className="flex justify-between pl-[24px] pr-[24px] mb=[14px]">
+                        <div className={styles.credit_div}>
+                            <p className={styles.credit_no}>5</p>
+                            <p style={{ fontSize: 12, fontWeight: 500 }} className={styles.credit}>Credits</p>
+                        </div>
+                        <Image
+                            src={profile}
+                            width={32}
+                            height={32}
+                            alt="Profile image"
+                            className="rounded-2xl"
+                        />
+                    </div>
+
+                    <div className="mx-auto w-full flex justify-center mb-[37px]">
+                        <Image src={logo} alt="logo" />
+                    </div>
+                    <div className="text-center text-[16px] font-[400]  text-white mb-[75px]">
+                        <p className="leading-[19.2px]">
+                            Hey <span className="font-[700]">Janis!</span> 👋{" "}
+                        </p>
+                        <p className="leading-[19.2px]">
+                            Unlock unique adventures tailored to your preferences. Are you Keen?
+                        </p>
+                    </div>
+                    <h3 className="text-[52px] ml-[24px] font-[600] leading-[62.4px] text-white max-w-[266px] ">
+                        Let’s <br />
+                        make memories!
+                    </h3>
+                </div>
+                <div className={styles.btn_main}>
+                    <div className={styles.btn_conatiner}>
+                        <div className="w-[342px] bg-white h-[72px]  right-0 left-[20px] rounded-[32px]  before:rounded-tr-[.5%] flex justify-center items-center px-[10px]">
+                            <button className="bg-[#DAF5FE] h-[52px] w-[320px] rounded-[32px] flex justify-center items-center gap-[5px] font-[500] text-[19px] leading-[23px]">
+                                Set up your profile
+                                <FaArrowRightLong size={13} />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className={`h-4/25  pb-[62px] ${styles.empty_container}`}>
+                    <p className={styles.edit_pref}>Edit preferences
+                        <Image
+                            src={mark}
+                            width={17}
+                            height={14}
+                            alt="mark image"
+                        />
+                    </p>
+                </div>
+                {/* </main> */}
+            </div>
+        </div>
+    );
+}
